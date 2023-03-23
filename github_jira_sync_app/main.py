252f3ee384c3e311ba7277d059713853da14996c
Maksim Beliaev <beliaev.m.s@gmail.com>
2023-03-23 17:53:38 +0100

    added settings added logger

--- github_jira_sync_app/main.py
+++ github_jira_sync_app/main.py
@@ -1,12 +1,41 @@
+import logging
 import os
 
 import requests
+import yaml
 from dotenv import load_dotenv
+from fastapi import Body
 from fastapi import FastAPI
 from github import Github
+from github import GithubException
 from github import GithubIntegration
+from yaml.scanner import ScannerError
 
 load_dotenv()
+
+with open("settings.yaml") as file:
+    DEFAULT_SETTINGS = yaml.safe_load(file)
+
+
+def define_logger():
+    """Define logger to output to the file and to STDOUT."""
+    log = logging.getLogger("api-demo-server")
+    log.setLevel(logging.DEBUG)
+    formatter = logging.Formatter(
+        fmt="%(asctime)s (%(levelname)s) %(message)s", datefmt="%d.%m.%Y %H:%M:%S"
+    )
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    log.addHandler(stream_handler)
+
+    log_file = os.environ.get("DEMO_SERVER_LOGFILE", "demo_server.log")
+    file_handler = logging.FileHandler(filename=log_file)
+    file_handler.setFormatter(formatter)
+    log.addHandler(file_handler)
+    return log
+
+
+logger = define_logger()
 
 
 app_id = os.getenv("APP_ID")
@@ -19,10 +48,22 @@ git_integration = GithubIntegration(
 app = FastAPI()
 
 
+def merge_dicts(d1, d2):
+    """Merge the two dictionaries (d2 into d1) recursively.
+
+    If the key from d2 exists in d1, then skip (do not override).
+
+    Mutates d1
+    """
+    for key in d2:
+        if key in d1 and isinstance(d1[key], dict) and isinstance(d2[key], dict):
+            merge_dicts(d1[key], d2[key])
+        elif key not in d1:
+            d1[key] = d2[key]
+
+
 @app.post("/")
-def bot(request):
-    # Get the event payload
-    payload = request.json
+def bot(payload: dict = Body(...)):
 
     # Check if the event is a GitHub PR creation event
     if not all(k in payload.keys() for k in ["action", "issue"]) and payload["action"] == "opened":
@@ -43,19 +84,32 @@ def bot(request):
         ).token
     )
     repo = git_connection.get_repo(f"{owner}/{repo_name}")
-    settings = repo.get_contents(".github/workflows/.jira_sync_config.yaml")
-    print(settings)
+    try:
+        settings_content = repo.get_contents(
+            ".github/workflows/.jira_sync_config55.yaml"
+        ).decoded_content
+    except GithubException:
+        logger.info("Settings file was not found, use default")
+        settings_content = b""
+
+    try:
+        settings = yaml.safe_load(settings_content)
+    except ScannerError:
+        logger.warning("YAML file is invalid, use default")
+        settings = {}
+
+    merge_dicts(settings, DEFAULT_SETTINGS)
+
+    settings = settings["settings"]
+
+    if not settings["add_comment"]:
+        return "ok"
 
     issue = repo.get_issue(number=payload["issue"]["number"])
 
-    # Call meme-api to get a random meme
     response = requests.get(url="https://picsum.photos/200/300")
-    # if response.status_code != 200:
-    #     return 'ok'
-
-    # Get the best resolution meme
     meme_url = response.url
-    # Create a comment with the random meme
+
     issue.create_comment(f"![Alt Text]({meme_url})")
     return "ok"
 
