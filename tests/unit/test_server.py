2b137952da29e000a11c9ca252a71720872f950f
Maksim Beliaev <beliaev.m.s@gmail.com>
2024-12-17 20:29:34 +0100

    Fix double webhook (#63)
    * fix #57: do not process opened issues which include labels
    * updated tests

--- tests/unit/test_server.py
+++ tests/unit/test_server.py
@@ -61,6 +61,17 @@ def test_comment_created_by_user(signature_mock):
 
 @responses.activate(assert_all_requests_are_fired=True)
 def test_issue_labeled_correct(signature_mock):
+    """Test the scenario when an existing bug is labeled with the right label.
+
+    Tests the following scenario:
+        1. Authenticate in GitHub
+        2. Get issue from GitHub
+        3. Get content of .jira_sync_config.yaml from GitHub repo
+        4. Ensure that the issue on GitHub is label with the approved label
+        5. Authenticate in Jira
+        6. Validate via JQL that this issue does not exist in Jira
+        7. Create new issue in Jira
+    """
     responses._add_from_file(UNITTESTS_DIR / "url_responses" / "issue_labeled_correct.yaml")
     responses._add_from_file(UNITTESTS_DIR / "url_responses" / "jira_jql_no_issues.yaml")
     responses._add_from_file(UNITTESTS_DIR / "url_responses" / "github_auth.yaml")
@@ -78,35 +89,23 @@ def test_issue_labeled_correct(signature_mock):
 
 @responses.activate(assert_all_requests_are_fired=True)
 def test_issue_created_with_label(signature_mock):
-    """Test the most common scenario when a bug is create on GitHub with the right label.
+    """Test the scenario when a bug is created on GitHub with the right label.
 
-    Tests the following scenario:
-        1. Authenticate in GitHub
-        2. Get issue from GitHub
-        3. Get content of .jira_sync_config.yaml from GitHub repo
-        4. Ensure that the issue on GitHub is label with the approved label
-        5. Authenticate in Jira
-        6. Validate via JQL that this issue does not exist in Jira
-        7. Create new issue in Jira
+    Ensure that we skip the processing, see
+    https://github.com/canonical/gh-jira-sync-bot/issues/57
     """
-    responses._add_from_file(UNITTESTS_DIR / "url_responses" / "github_auth.yaml")
-    responses._add_from_file(UNITTESTS_DIR / "url_responses" / "github_settings_with_labels.yaml")
-    responses._add_from_file(UNITTESTS_DIR / "url_responses" / "jira_auth_responses.yaml")
-    responses._add_from_file(UNITTESTS_DIR / "url_responses" / "issue_labeled_correct.yaml")
-    responses._add_from_file(UNITTESTS_DIR / "url_responses" / "jira_jql_no_issues.yaml")
-    responses._add_from_file(UNITTESTS_DIR / "url_responses" / "jira_create_issue.yaml")
     response = client.post(
         "/",
         json=_get_json("issue_created_with_label.json"),
     )
 
     assert response.status_code == 200
-    assert response.json() == {"msg": "Issue was created in Jira. "}
+    assert "Action was triggered by Issue Opened with Labels." in response.json()["msg"]
 
 
 @responses.activate(assert_all_requests_are_fired=True)
-def test_issue_created_with_label_for_existing_ticket(signature_mock):
-    """Test the scenario when a bug is created on GitHub with the right label
+def test_issue_labeled_for_existing_ticket(signature_mock):
+    """Test the scenario when a bug is labeled with the right label
     but the issue already exists in Jira.
 
     Tests the following scenario:
@@ -125,7 +124,7 @@ def test_issue_created_with_label_for_existing_ticket(signature_mock):
     responses._add_from_file(UNITTESTS_DIR / "url_responses" / "jira_auth_responses.yaml")
     response = client.post(
         "/",
-        json=_get_json("issue_created_with_label.json"),
+        json=_get_json("issue_labeled_correct.json"),
     )
 
     assert response.status_code == 200
